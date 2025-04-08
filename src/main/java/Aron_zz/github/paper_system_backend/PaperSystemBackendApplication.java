57f7d50c1888c3291c0ab5011cb973fdf91d336d
zhangxingyan <2356826696@qq.com>
2025-04-08 21:48:03 +0800

    init_backend

---
+++ src/main/java/Aron_zz/github/paper_system_backend/PaperSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package Aron_zz.github.paper_system_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaperSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Aron_zz/github/paper_system_backend/PaperSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Aron_zz.github.paper_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
